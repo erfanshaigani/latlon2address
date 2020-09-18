8063ef3cec9c585ff70af99b3ac339c61983dd16
erfanshaigani <55313985+erfanshaigani@users.noreply.github.com>
2020-09-18 11:46:29 +0430

    Update address.php

--- address.php
+++ address.php
@@ -1,7 +1,7 @@
 <?php
 
-$address = file_get_contents("http://62.193.6.32:5000/?lat=35.722848&lon=51.346150");
-//$address = file_get_contents("62.193.6.32:5000/?lat=35.722848&lon=51.346150");
+$address = file_get_contents("http://62.X.X.X:5000/?lat=35.722848&lon=51.346150");
+//$address = file_get_contents("62.X.X.X:5000/?lat=35.722848&lon=51.346150");
 //be careful, it doesnt work!
 //u have to specify the protocol! (http,ftp, ....)
 echo $address;
